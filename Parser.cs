cd13c5844c29a6bc0889681be3d12105c9395f68
konsowa <mostakons@gmail.com>
2015-12-06 15:17:54 +0200

    Update Parser.cs

--- Parser.cs
+++ Parser.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,20 +17,17 @@ namespace MipSim
                 //Using @ before the string disables string character escaping preventing the need to use double \ to escape regex characters
                 //Rd is limited 1-15 since we shouldn't be able to write to $0
                 @"^(add|xor|slt) \$([1-9]|1[0-5]),\s?\$([0-9]|1[0-5]),\s?\$([0-9]|1[0-5])$",
-
-                //TODO: Do the same as the above pattern
-                "^addi (\\$([0-9]|[1-2][0-9]|3[0-1])),\\s?(\\$([0-9]|[1-2][0-9]|3[0-1])),\\s?[0-9]+$",
-                "^(sw|lw) (\\$([0-9]|[1-2][0-9]|3[0-1])),\\s?[0-9]+\\((\\$([0-9]|[1-2][0-9]|3[0-1]))\\)$",
-                "^(j|jal|jp) (([0-9]*[A-Za-z]*)+|[A-Za-z]+[0-9]*)$",
-                "^jr (\\$(([1-2][0-9]|3[0-1])|[0-9]))$",
-                "^ble (\\$([0-9]|[1-2][0-9]|3[0-1])),\\s?(\\$([0-9]|[1-2][0-9]|3[0-1])),\\s?(([0-9]*[A-Za-z]*)+|[A-Za-z]+[0-9]*)$",
+                @"^addi \$([1-9]|1[0-5]),\s?\$([0-9]|1[0-5]),\s?[0-9]+$",
+                @"^(sw|lw) \$([0-9]|1[0-5]),\s?([0-9]+)\(\$([1-9]|1[0-5])\)$",
+                "^(j|jal|jp) ([0-9]+)$",
+                @"^jr \$([0-9]|1[0-5])$",
+                @"^ble (\$([0-9]|1[0-5])),\s?\$([0-9]|1[0-5]),\s?([0-9]+)$",
                 "^rp$"
             };
 
             instruction = instruction.ToLower().Trim();
 
             //Syntax check
-            //bool validInsruction = false;
 
             for (int i = 0; i < patterns.Length; i++)
 			{
@@ -49,17 +46,23 @@ namespace MipSim
                                 return new Slt(instruction, instructionNumber, int.Parse(match.Groups[2].Value), int.Parse(match.Groups[3].Value), int.Parse(match.Groups[4].Value));
                             break;
                         case 1:
-                            //TODO: Konsowa or Hazem
-                            break;
+                            return new Addi(instruction, instructionNumber, int.Parse(match.Groups[2].Value), int.Parse(match.Groups[3].Value), int.Parse(match.Groups[4].Value));
                         case 2:
-                            //TODO: Konsowa or Hazem
+                            if (match.Groups[1].Value == "sw")
+                                return new SW(instruction, instructionNumber, int.Parse(match.Groups[2].Value), int.Parse(match.Groups[3].Value), int.Parse(match.Groups[4].Value));
+                            if (match.Groups[1].Value == "lw")
+                                return new LW(instruction, instructionNumber, int.Parse(match.Groups[2].Value), int.Parse(match.Groups[3].Value), int.Parse(match.Groups[4].Value));
                             break;
                         case 3:
-                            //TODO: Konsowa or Hazem
+                            if (match.Groups[1].Value == "j")
+                                return new J(instruction, instructionNumber, int.Parse(match.Groups[2].Value));
+                            if (match.Groups[1].Value == "jal")
+                                return new Jal(instruction, instructionNumber, int.Parse(match.Groups[2].Value));
+                            if (match.Groups[1].Value == "jp")
+                                return new JumpProcedure(instruction, instructionNumber, int.Parse(match.Groups[2].Value));
                             break;
                         case 4:
-                            //TODO: Konsowa or Hazem
-                            break;
+                            return new JR(instruction, instructionNumber, int.Parse(match.Groups[2].Value));
                         case 5:
                             //TODO: Konsowa or Hazem
                             break;
@@ -67,64 +70,10 @@ namespace MipSim
                             return new ReturnProcedure(instruction, instructionNumber);
                     }
 
-                    //validInsruction = true;
-
                     break;
                 }
 
 			}
-
-            /*string instrName = "";
-
-            if (validInsruction)
-	        {
-                
-
-                if (instruction.Trim() != "rp")
-                    instrName = instruction.Split(' ')[0];
-                else
-                    instrName = instruction;
-
-                switch (instrName)
-	            {
-                    
-                    case "add":
-                        int rd = int.Parse(instruction.Split('$')[1].Split(',')[0].Trim());
-                        int rs = int.Parse(instruction.Split(',')[1].Trim('$'));
-                        int rt = int.Parse(instruction.Split(',')[2].Trim());
-                        Add add = new Add(instruction, instructionNumber, rd, rs, rt);
-                        break;
-                    case "lw":
-                        int imm = int.Parse(instruction.Split(',')[1].Split('(')[0].Trim());
-                        rs = int.Parse(instruction.Split(',')[1].Split('(')[1].Trim('$',')'));
-                        rt = int.Parse(instruction.Split('$')[1].Split(',')[0].Trim());
-                        LW lw = new LW(instruction, instructionNumber, rt, imm, rs);
-                        break;
-                    case "sw":
-                        imm = int.Parse(instruction.Split(',')[1].Split('(')[0].Trim());
-                        rs = int.Parse(instruction.Split(',')[1].Split('(')[1].Trim('$',')'));
-                        rt = int.Parse(instruction.Split('$')[1].Split(',')[0].Trim());
-                        SW sw = new SW(instruction, instructionNumber, rt, imm, rs);
-                        break;
-                    case "jr":
-                        rs = int.Parse(instruction.Split('$')[1].Trim());
-                        JR jr = new JR(instruction, instructionNumber, rs);
-                        break;
-                    case "j":
-                        int address = int.Parse(instruction.Split(' ')[1].Trim());
-                        J j = new J(instruction,instructionNumber,address);
-                        break;
-                    case "jp":
-                        address = int.Parse(instruction.Split(' ')[1].Trim());
-                        JumpProcedure jp = new JumpProcedure(instruction, instructionNumber, address);
-                        break;
-                    case "rp":
-                        ReturnProcedure rp = new ReturnProcedure(instruction, instructionNumber);
-                        break;
-		            default:
-                        throw new ParserException("invalid instruction called " + instrName);
-	            }
-	        }*/
 
             throw new ParserException("Invalid instruction");
         }
